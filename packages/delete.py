2911806b4e210a7fe43ad2807db56ae30854c531
obliqueasymptote <userinterfacey@tutanota.com>
2025-08-06 01:45:08 -0400

    so what i want u to do, is ignorehow messy it is, yes i added exponential back off, yes i randomly wrote some code in absolutely no local scope, i was trying to figure out the best way to repeatedly delete messages and its almost 2am thanks

--- packages/delete.py
+++ packages/delete.py
@@ -1,12 +1,12 @@
+import random
 import logging
 from logging import Logger
 
 import requests
-from enum import Enum
 from time import sleep
 from requests import Response
 
-from typing import Union, Type, Optional, Any
+from typing import Union, Type, Optional, Any, Tuple
 
 logging.basicConfig(
     level=logging.WARNING,
@@ -14,32 +14,58 @@ logging.basicConfig(
 )
 error_logger: Logger = logging.getLogger(__name__)
 
-"""dont know why i needed this since the requests module has built in status code attributes but its ok maybve i will use it later meow"""
-class HttpCodes(Enum):
-    SUCCESS = 200
-    BAD_REQUEST = 400
-    UNAUTHORIZED = 401
-    FORBIDDEN = 403
-    NOT_FOUND = 404
-    RATE_LIMIT = 429
-    INTERNAL_SERVER_ERROR = 500
-
 class MessageDeleter:
+    
+    def __init__(self, token: str, channel_id: str):
+        self.token = token
+        self.channel_id = channel_id
 
-    token: str = ""
+    def exponential_backoff(self, retry_after: float, attempt: int) -> float:
+        '''me when the CS lesson on time complexities comes in handy pray emoji
+        exponential vs quadratic head ahh no srsly i actualy confused 2^n and n^2'''
+        return (retry_after / 1000.0) * (2 ** attempt)
 
-    @classmethod
-    def set_token(cls: Type['MessageDeleter'], token: str):
-        cls.token = token
-
-
-    @classmethod
-    def get_msg_batch(cls: Type['MessageDeleter'], channel_id: str) -> Optional[str]:
+    def get_first_id(self) -> str:
         message_response: Response = requests.get(
-            url=f"https://discord.com/api/v9/channels/{channel_id}/messages",
-            headers={"Authorization": cls.token}
+            url=f"https://discord.com/api/v9/channels/{self.channel_id}/messages",
+            headers={"Authorization": self.token}
         )
-        if not message_response.ok:
-            error_logger.warning(f"[{message_response.status_code}] - {message_response.reason}")
-            return
-        return message_response.json()[-1]['id']
+        return message_response.json()[0]['id']
+
+    def delete_message(self) -> None:
+        current_id = self.get_first_id()
+
+        while True:
+            delete_request: Response = requests.delete(
+                url=f"https://discord.com/api/v9/channels/{self.channel_id}/messages/{current_id}",
+                params={'before': current_id},
+                headers={"Authorization": self.token}
+            )
+            if not delete_request.ok:
+                error_count: int = 0
+                if delete_request.status_code == 429:
+                    wait_time: Any = delete_request.headers.get("retry-after")
+                    error_logger.warning(f"Rate limit hit- waiting {wait_time} seconds")
+                    error_count+= 1
+                    if error_count == random.randrange(3, 6):
+                        sleep(self.exponential_backoff(wait_time, error_count))
+                    else:
+                        sleep(wait_time)
+                else:
+                    error_logger.error(f"{delete_request.status_code} - {delete_request.reason.upper()}")
+
+
+
+
+
+
+while True:
+    message_response: Response = requests.get(
+                url=f"https://discord.com/api/v9/channels/{self.channel_id}/messages",
+                headers={"Authorization": self.token}
+            )
+    for message in message_response.json():
+        requests.delete(
+            url=f"https://discord.com/api/v9/channels/{self.channel_id}/messages/{message_response['id']}",
+            headers={"Authorization": self.token}
+        )
