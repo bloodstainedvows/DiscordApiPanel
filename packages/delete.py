81d41e9a8173b6db8df51a6f7f7fb37447dcafd5
obliqueasymptote <userinterfacey@tutanota.com>
2025-08-07 02:31:13 -0400

    ok. last time im being schizophrenic. i was so confused why i kept getting a 403, i was trying to delete every fucking message ever, not just my own. then learned knock knock go to that endpoint and get ur own id! silly girl, okay i promise it works now i promise pinky, any commit from here on out is going to be me cleaning up any messes and just organising the code to be prettier <3

--- packages/delete.py
+++ packages/delete.py
@@ -22,6 +22,12 @@ class MessageDeleter:
         self.base_endpoint: str = f"https://discord.com/api/v9/channels/{self.channel_id}/messages"
         self.rate_count: int = 0
 
+        self.user_id: str = requests.get(
+            url=f"https://discord.com/api/v10/users/@me",
+            headers={"Authorization": self.token}
+        ).json()['id']
+        
+
     def exponential_backoff(self, retry_after: float, attempt: int) -> float:
         '''me when the CS lesson on time complexities comes in handy pray emoji
         exponential vs quadratic head ahh no srsly i actualy confused 2^n and n^2'''
@@ -36,9 +42,9 @@ class MessageDeleter:
             retry_header: Any = response.headers.get('retry-after')
            
             if self.rate_count == random.randrange(3, 6):
-                sleep(self.exponential_backoff(int(retry_header), self.rate_count))
+                sleep(self.exponential_backoff(float(retry_header), self.rate_count))
             else:
-                sleep(int(retry_header))
+                sleep(float(retry_header))
             return
         error_logger.error(f'{response.status_code} - {response.reason}')
 
@@ -55,7 +61,9 @@ class MessageDeleter:
                 print("All messages have been deleted\n")
                 break
             
-            message_ids: List[str] = [message['id'] for message in json_response]
+            user_msgs: List[str] = [message for message in json_response if message['author']['id'] == self.user_id]
+            message_ids: List[str] = [message['id'] for message in user_msgs]
+            
             for id in message_ids:
                 delete_request: Response = requests.delete(
                     url=f"{self.base_endpoint}/{id}",
