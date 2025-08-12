aa9cb3dd4eab7904fdfe9a4bc4a17e6606021d00
obliqueasymptote <userinterfacey@tutanota.com>
2025-08-11 23:53:04 -0400

    It's much quicker to wait a second or two between each message so the deletion rate is more consistent before implementing exponential backoff

--- packages/ratelimit_bypass.py
+++ packages/ratelimit_bypass.py
@@ -13,12 +13,13 @@ def exponential_backoff(retry_after: float, attempt: int) -> float:
 
 def handle_ratelimit(response: Response, rate_count: int) -> None:
     if response.ok:
+        sleep(1)
         return
     if response.status_code == 429:
         rate_count += 1
         retry_header: Any = response.headers.get('retry-after')
         
-        if rate_count == random.randrange(3, 8):
+        if rate_count == random.randrange(3, 5):
             sleep(exponential_backoff(float(retry_header), rate_count))
         else:
             sleep(float(retry_header))
