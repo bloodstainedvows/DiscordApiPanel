31f97bba04536c2090752b772ec4d2a4d7500ffc
vixenkills <convolutionarynodes@proton.me>
2025-08-18 23:22:37 -0400

    refactored a bit to avoid redundancy

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-
 import os
 import sys
 import logging
@@ -6,7 +5,7 @@ from logging import Logger
 
 from typing import Optional
 
-from packages import MessageDeleter
+from packages import *
 from dotenv import load_dotenv
 
 load_dotenv(verbose=True)
@@ -17,19 +16,32 @@ logging.basicConfig(
     )
 error_logger: Logger = logging.getLogger(__name__)
 
+def valid_channel(channel_id: str) -> bool:
+    if not channel_id:
+        error_logger.error('Channel ID cannot be empty')
+        return False
+    return True
+
+def setup_deleter(token: str) -> None:
+    channel_id: str = input("\nPlease enter the channel ID of the dms you'd like to wipe:\n")
+    if not valid_channel(channel_id):
+        return
+    deleter: MessageDeleter = MessageDeleter(token, channel_id)
+    deleter.delete_messages()
+
+def setup_spammer(token: str) -> None:
+    channel_id: str = input("Please enter the channel ID of the dms you'd like to spam:\n")
+    if not valid_channel(channel_id):
+        return
+    spammer: MessageSpammer = MessageSpammer(token, channel_id)
+    spammer.spam_message(input("\nWhat would you like to spam:\n"))
+
 
 def main():
     auth_token: Optional[str] = os.getenv("TOKEN")
     if not auth_token:
         error_logger.error("[!] Token cannot be found\n[!] Please ensure to create a .env in the discordApiPanel folder with the format:\n\t'TOKEN'=YOUR TOKEN")
         sys.exit(1)
-    channel_id: str = input("Please enter the channel ID of the dms you'd like to wipe:\n")
-    if not channel_id:
-        error_logger.error('Channel ID cannot be empty')
-        sys.exit(1)
-    print("Starting deletion..\n")
-    deleter: MessageDeleter = MessageDeleter(auth_token, channel_id)
-    deleter.delete_messages()
 
 
 if __name__ == "__main__":
